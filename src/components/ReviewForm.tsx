c3c78c887c9f7630cf0b288dad1106f546505da1
Revel <revelcahyadi2004@gmail.com>
2025-01-21 23:07:42 +0700

    CSS finally

--- src/components/ReviewForm.tsx
+++ src/components/ReviewForm.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Question } from "../interface/Question.interfaces";
 import "../style/ReviewForm.css";
 const ReviewForm: React.FC = () => {
   const location = useLocation();
   const questions: Question[] = location.state?.questions || [];
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   if (questions.length === 0) {
     return <p>No questions to review.</p>;
@@ -13,34 +15,39 @@ const ReviewForm: React.FC = () => {
   const handleSubmitReview = (e: React.FormEvent) => {
     e.preventDefault();
     console.log("Review Submitted");
+    setIsSubmitted(true);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmitReview}>
-        <h2>Review Questions</h2>
-        {questions.map((question) => (
-          <div key={question.id}>
-            <p>
-              <strong>Question {question.id}:</strong>
-              {question.content}
-            </p>
-            <p>
-              <strong>Score:</strong> {question.score}
-            </p>
-            <label>Comments:</label>
-            <textarea placeholder="Enter your comment" />
-            <label>
-              Approve:
-              <select>
-                <option value="false">Not Approved</option>
-                <option value="true">Approved</option>
-              </select>
-            </label>
-          </div>
-        ))}
-        <button type="submit">Submit Review</button>
-      </form>
+      {isSubmitted ? (
+        <h2>Thank you for your time</h2>
+      ) : (
+        <form onSubmit={handleSubmitReview}>
+          <h2>Review Questions</h2>
+          {questions.map((question) => (
+            <div className="review-div" key={question.id}>
+              <p>
+                <strong>Question {question.id}:</strong>
+                {question.content}
+              </p>
+              <p>
+                <strong>Score:</strong> {question.score}
+              </p>
+              <label>Comments:</label>
+              <textarea placeholder="Enter your comment" />
+              <label>
+                Approve:
+                <select>
+                  <option value="false">Not Approved</option>
+                  <option value="true">Approved</option>
+                </select>
+              </label>
+            </div>
+          ))}
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 };
